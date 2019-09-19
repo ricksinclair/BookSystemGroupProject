6c04d58cc9f0a0020fb8ddfc0ea464a593b645c9
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-09-19 11:17:20 -0400

    adding http status codes to controller

--- note-service/src/main/java/com/trilogyed/noteservice/controller/NoteServiceController.java
+++ note-service/src/main/java/com/trilogyed/noteservice/controller/NoteServiceController.java
@@ -4,6 +4,7 @@ import com.trilogyed.noteservice.dao.NoteDao;
 import com.trilogyed.noteservice.model.Note;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,6 +21,7 @@ public class NoteServiceController {
     }
 
     @RequestMapping(value="/notes", method= RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Note addNote(@RequestBody @Valid Note note) {
         Note exists = noteDao.getNoteById(note.getNoteId());
         if(exists != null)
@@ -29,6 +31,7 @@ public class NoteServiceController {
     }
 
     @RequestMapping(value ="/notes/{id}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
     public Note getNoteById(@PathVariable (name="id") int noteId) {
         try {
             return noteDao.getNoteById(noteId);
@@ -38,21 +41,25 @@ public class NoteServiceController {
     }
 
     @RequestMapping(value="/notes/book/{bookId}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Note> getNotesByBook(@PathVariable(name="bookId") int bookId) {
         return noteDao.getNotesByBook(bookId);
     }
 
     @RequestMapping(value="/notes", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Note> getAllNotes() {
         return noteDao.getAllNotes();
     }
 
     @RequestMapping(value="/notes/{id}", method= RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK)
     public void updateNote(@RequestBody Note note){
         noteDao.updateNote(note);
     }
 
     @RequestMapping(value="/notes/{id}", method=RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteNote(@PathVariable(name="id") int noteId) {
         noteDao.deleteNote(noteId);
     }
